c9ad2bca94ad436acf69d4e101297c8c8c9d69f2
kleos <36254493+amlitio@users.noreply.github.com>
2023-12-08 21:53:14 -0500

    Update content_generator.py

--- src/content_generator.py
+++ src/content_generator.py
@@ -1,1 +1,27 @@
+import openai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+def generate_peptide_content(summary, platform='blog'):
+    if platform == 'twitter':
+        tweet_prompt = f"Create an engaging tweet about this peptide research finding:\n\n{summary}"
+        max_length = 280
+    else:
+        article_prompt = f"Write an informative short article about these peptide research findings:\n\n{summary}"
+        max_length = 500
+
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=(tweet_prompt if platform == 'twitter' else article_prompt),
+        max_tokens=max_length
+    )
+    return response.choices[0].text.strip()
+
+# Example usage
+# summary = "Summary of the peptide article"
+# content_for_twitter = generate_peptide_content(summary, 'twitter')
+# content_for_blog = generate_peptide_content(summary, 'blog')
 
